514d0a9ae34600155c3b90b102f3345592773eef
Ming <khgfe2002@gmail.com>
2021-07-04 11:58:32 +0800

    首頁按鈕設計

--- src/views/frontend/Index.vue
+++ src/views/frontend/Index.vue
@@ -21,8 +21,12 @@
           <div class="features_icon"></div>
           <h3>每天都可以喝的味道</h3>
           <p>
-            選用優質的生豆，用小火慢慢煮，以促進美味成分的化學變化。<br />
-            每天都可以喝的咖啡不需要第一口就很香。它並不華麗，如果您注意到它，將永遠不會厭倦它。這就是味道。
+            <span>
+              選用優質的生豆，用小火慢慢煮，以促進美味成分的化學變化。
+            </span>
+            <span>
+              每天都可以喝的咖啡不需要第一口就很香。它並不華麗，如果您注意到它，將永遠不會厭倦它。這就是味道。
+            </span>
           </p>
         </div>
       </div>
@@ -34,9 +38,20 @@
           <div class="features_icon"></div>
           <h3>每天喝得起的價格</h3>
           <p>
-            生咖啡豆的市場價格由供求關係決定。稀缺豆價值高、品質好的生豆價格也會相應上漲。<br />
-            採購標準是質量好、產量穩定的生豆。我們實現了既滿足顧客可以飲用的價格又滿足保持商店營業的利潤的平衡。
+            <span>
+              生咖啡豆的市場價格由供求關係決定。稀缺豆價值高、品質好的生豆價格也會相應上漲。
+            </span>
+            <span>
+              採購標準是質量好、產量穩定的生豆。我們實現了既滿足顧客可以飲用的價格又滿足保持商店營業的利潤的平衡。
+            </span>
           </p>
+          <div class="mybtn">
+            <a class="mybtn_link" href="">
+              <span>
+                查看商品
+              </span>
+            </a>
+          </div>
         </div>
       </div>
     </section>
@@ -74,38 +89,110 @@
 }
 .features {
   display: flex;
-}
-.features:nth-of-type(3) {
-  flex-direction: row-reverse;
-}
-.features_img {
-  width: 50%;
-  line-height: 0;
-  border-radius: 20px;
-  overflow: hidden;
-}
-.features_txt {
-  width: 50%;
-  padding: 0 6%;
-  text-align: left;
-  display: flex;
   flex-wrap: wrap;
-  align-content: center;
+  &:nth-of-type(3) {
+    flex-direction: row-reverse;
+  }
+  &_img {
+    width: 50%;
+    line-height: 0;
+    & img {
+      border-radius: 20px;
+    }
+  }
+  &_txt {
+    width: 50%;
+    padding: 5% 6%;
+    text-align: left;
+    & h3 {
+      margin-bottom: 15px;
+      font-weight: 600;
+      width: 100%;
+    }
+    & p span {
+      display: block;
+      margin-top: 10px;
+    }
+  }
+  &_icon::before {
+    display: flex;
+    justify-self: center;
+    align-items: center;
+    width: 60px;
+    height: 60px;
+    background-color: #f5eadb;
+    content: '';
+    margin-bottom: 20px;
+    border-radius: 50%;
+  }
 }
-.features_txt h3 {
-  margin-bottom: 25px;
-  font-weight: 600;
-  width: 100%;
-}
-.features_icon::before {
+// 按鈕
+.mybtn {
   display: flex;
-  justify-self: center;
-  align-items: center;
-  width: 60px;
-  height: 60px;
-  background-color: #fff2bc;
-  content: '';
-  margin-bottom: 20px;
-  border-radius: 50%;
+  justify-content: center;
+  &_link:link,
+  &_link:visited {
+    text-decoration: none;
+    display: block;
+    position: relative;
+    z-index: 1;
+    width: 114px;
+    transition: all 0.6;
+  }
+  &_link span {
+    color: #fff;
+    display: block;
+    padding: 8px 25px;
+    background-color: #debc8c;
+    text-align: center;
+    position: absolute;
+    transition: all 0.3s;
+    left: 0;
+    top: 0;
+    width: 100%;
+  }
+  &_link:hover span {
+    top: 6px;
+    width: 112%;
+    left: -6%;
+    color: #000;
+  }
+  &_link::after {
+    position: absolute;
+    content: '';
+    width: 112%;
+    z-index: -1;
+    border: 1px solid #debc8c;
+    height: 42px;
+    left: -6%;
+    top: 5px;
+  }
+}
+@media only screen and (max-width: 1200px) {
+  .features {
+    &_txt {
+      width: 50%;
+      padding: 3%;
+      text-align: left;
+    }
+  }
+}
+@media only screen and (max-width: 1024px) {
+  .features {
+    &_img {
+      width: 100%;
+    }
+    &_txt {
+      width: 100%;
+      padding: 3%;
+      text-align: left;
+      max-width: 640px;
+      margin: 0 auto;
+    }
+    &_icon::before {
+      width: 40px;
+      height: 40px;
+    }
+  }
 }
 </style>
